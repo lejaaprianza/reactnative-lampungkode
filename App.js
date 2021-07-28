ac6802badc1bb322123ca4e1f3bf4c0b0c220c8c
lejaaprianza <lejaprianza@gmail.com>
2021-07-28 09:44:04 +0700

    tutorial 2, Text Component

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontStyle: "normal",
+          textAlign: "right",
+          letterSpacing: 5,
+          color: "orange",
+          fontSize: 28,
+        }}
+      >
+        Hello World
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
