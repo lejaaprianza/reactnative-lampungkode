66388a9eb19cf282701faaadb6ceef4ea3a57f5c
lejaaprianza <lejaprianza@gmail.com>
2021-08-14 23:23:43 +0700

    tutorial 4

--- App.js
+++ App.js
@@ -1,49 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from "react"
+import { Text, View, StyleSheet } from "react-native"
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
-      <View
-        style={{
-          // width: 100,
-          // height: 100,
-          flexDirection: "column",
-          flex: 0.3,
-          backgroundColor: "pink",
-          margin: 20,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+    <View style={gaya.State1}>
+      <View style={gaya.State2}>
         <Text>1</Text>
         <Text>2</Text>
         <Text>3</Text>
         <Text>4</Text>
         <Text>5</Text>
       </View>
-      <View
-        style={{
-          // width: 100,
-          // height: 100,
-          flex: 0.3,
-          backgroundColor: "red",
-          marginHorizontal: 20,
-          marginVertical: 20,
-          paddingHorizontal: 20,
-          paddingVertical: 20,
-          justifyContent: "flex-end",
-          alignItems: "flex-end",
-        }}
-      >
+      <View style={gaya.State3}>
         <Text>Contoh 2</Text>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
+const gaya = StyleSheet.create({
+  State1: {
+    flex: 1,
+  },
+  State2: {
+    flexDirection: "column",
+    flex: 0.3,
+    backgroundColor: "pink",
+    margin: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  State3: {
+    flex: 0.3,
+    backgroundColor: "red",
+    marginHorizontal: 20,
+    marginVertical: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+})
