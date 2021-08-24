28bd7a5b1501a8aea0f691349063cf5e3f88384f
lejaaprianza <lejaprianza@gmail.com>
2021-08-24 21:16:52 +0700

    tutorial 7

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from "react"
-import { View, StyleSheet } from "react-native"
-import Inputs from "./Inputs"
+import { View, Image, Text } from "react-native"
 
 const App = () => {
   return (
     <View>
-      <Inputs />
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Text>Ini Gambar dari folder images</Text>
+        <Image
+          source={require("./images/react-native-logo.png")}
+          style={{
+            width: 260,
+            height: 300,
+          }}
+        />
+        <View style={{ height: 50 }} />
+        <Text>Ini Gambar dari URI</Text>
+        <Image
+          source={{
+            uri: "https://lampungkode.id/wp-content/uploads/2021/07/lampung-kode-1.png",
+          }}
+          style={{
+            width: 260,
+            height: 50,
+          }}
+        />
+      </View>
     </View>
   )
 }
