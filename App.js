11d756cca187e02c9af63ae8d9c73c84b63912eb
lejaaprianza <lejaprianza@gmail.com>
2021-09-03 15:57:46 +0700

    tutorial-9

--- App.js
+++ App.js
@@ -1,39 +1,7 @@
-import React from "react"
-import {
-  Button,
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Alert,
-} from "react-native"
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-  },
-  text: {
-    padding: 25,
-    color: "white",
-    backgroundColor: "green",
-  },
-})
-
-const App = () => {
-  return (
-    <>
-      <Button
-        onPress={() => Alert.alert("Contoh Button 1")}
-        title="Contoh Button 1"
-        color="blue"
-      />
-      <View style={{ height: 80 }} />
-      <View style={styles.container}>
-        <TouchableOpacity onPress={() => Alert.alert("Contoh Button 2")}>
-          <Text style={styles.text}>Contoh Button 2</Text>
-        </TouchableOpacity>
-      </View>
-    </>
-  )
+import React, { Component } from "react"
+import { WebView } from "react-native-webview"
+export default class App extends Component {
+  render() {
+    return <WebView source={{ uri: "https://lampungkode.id/" }} />
+  }
 }
-export default App
